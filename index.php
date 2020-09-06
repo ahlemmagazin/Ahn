0a52ec128d598eba4ad5393dbee3d822333f7406
rosa81 <70821062+rosa81@users.noreply.github.com>
2020-09-06 17:17:55 +0200

    upload site

---
+++ index.php
@@ -0,0 +1,41 @@
+
+    
+
+
+    <?php
+    
+    $conn=("localhost","id14790045_ah","%OW>8cT]SHkVbL/y","id14790045_lam	");
+
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+
+if(isset($_POST['submit']))
+{
+
+    echo "your email :".$email;
+    echo "<br>";
+    echo " your password:".$password;
+    echo" <h1> your informatios is correct</h1>";
+    echo" <h1> now any news you will receive</h1>";
+
+    
+
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
